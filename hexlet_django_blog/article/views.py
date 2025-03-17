298ac8364d1750a3c71aa6bcd27d5171d84300cb
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-03-17 21:32:44 +0300

    Add separate template for article index view

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'articles/index.html', context={
+        'app_name': 'Articles',
+    })
