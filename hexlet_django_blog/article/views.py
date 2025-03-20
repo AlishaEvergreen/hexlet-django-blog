6c1137924a7afeb2cba4f05ddc242ee910e8cfbe
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-03-20 21:15:56 +0300

    Refactor views: Convert index views to class-based views (CBV)

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    return render(request, 'articles/index.html', context={
-        'app_name': 'Articles',
-    })
+class ArticleIndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'articles/index.html', context={
+            'app_name': 'Articles',
+        })
