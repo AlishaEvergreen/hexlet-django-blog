68721e0a038fbacf15606f645000e4464603bc45
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-03-28 23:29:46 +0300

    Add Django admin interface for Article model

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Article
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ['title', 'content']
+    list_filter = (('created_at', DateFieldListFilter),)
