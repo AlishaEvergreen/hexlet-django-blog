63762615a744c148ef830bc85034d5acc94fa5e2
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-04-04 22:38:58 +0300

    Make article titles clickable and sort articles by ascending ID

--- hexlet_django_blog/articles/views.py
+++ hexlet_django_blog/articles/views.py
@@ -1,30 +1,33 @@
-from django.shortcuts import render
+# from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import View, UpdateView, DetailView
+from django.views.generic import ListView, UpdateView, DetailView
 from .models import Article
 from .forms import ArticleForm
 
 
-class IndexView(View):
+class IndexView(ListView):
+    model = Article
+    template_name = 'articles/index.html'
+    context_object_name = 'articles'
 
-    def get(self, request, *args, **kwargs):
-        articles = Article.objects.all()[:15]
-        return render(request, 'articles/index.html', context={
-            'app_name': 'Articles',
-            'articles': articles,
-        })
+    def get_queryset(self):
+        return Article.objects.order_by("id")[:15]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['app_name'] = 'Articles'
+        return context
+
 
 # А можно и так:
-# class IndexView(ListView):
-#     model = Article
-#     template_name = 'articles/index.html'
-#     context_object_name = 'articles'
-#     queryset = Article.objects.all()[:15]
+# class IndexView(View):
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['app_name'] = 'Articles'
-#         return context
+#     def get(self, request, *args, **kwargs):
+#         articles = Article.objects.all()[:15]
+#         return render(request, 'articles/index.html', context={
+#             'app_name': 'Articles',
+#             'articles': articles,
+#         })
 
 
 class ArticleUpdate(UpdateView):
@@ -39,3 +42,12 @@ class ArticleUpdate(UpdateView):
 class ArticleDetail(DetailView):
     model = Article
     template_name = 'articles/detail.html'
+
+
+# А можно и так:
+# class ArticleDetail(View):
+#     def get(self, request, *args, **kwargs):
+#         article = get_object_or_404(Article, pk=kwargs['pk'])
+#         return render(request, 'articles/detail.html', context={
+#             'article': article,
+#         })
